4834e7d107754b0db1e420f234789963af56edc8
lizhuoyou <2938093188@qq.com>
2024-07-04 09:06:46 +0800

    Init:

---
+++ VirtualStock/build/Desktop_Qt_6_5_3_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,298 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.5.3
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStackedWidget>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QStackedWidget *selectpage;
+    QWidget *selectpage1;
+    QGroupBox *groupBox;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *firstbutton1;
+    QPushButton *stockbutton1;
+    QPushButton *rankbutton1;
+    QPushButton *dealbutton1;
+    QPushButton *communitybutton1;
+    QWidget *selectpage2;
+    QGroupBox *groupBox_2;
+    QHBoxLayout *horizontalLayout_2;
+    QPushButton *firstbutton2;
+    QPushButton *stockbutton2;
+    QPushButton *rankbutton2;
+    QPushButton *dealbutton2;
+    QPushButton *communitybutton2;
+    QWidget *selectpage3;
+    QGroupBox *groupBox_3;
+    QHBoxLayout *horizontalLayout_3;
+    QPushButton *firstbutton3;
+    QPushButton *stockbutton3;
+    QPushButton *rankbutton3;
+    QPushButton *dealbutton3;
+    QPushButton *communitybutton3;
+    QWidget *selectpage4;
+    QGroupBox *groupBox_4;
+    QHBoxLayout *horizontalLayout_4;
+    QPushButton *firstbutton4;
+    QPushButton *stockbutton4;
+    QPushButton *rankbutton4;
+    QPushButton *dealbutton4;
+    QPushButton *communitybutton4;
+    QWidget *selectpage5;
+    QGroupBox *groupBox_5;
+    QHBoxLayout *horizontalLayout_5;
+    QPushButton *firstbutton5;
+    QPushButton *stockbutton5;
+    QPushButton *rankbutton5;
+    QPushButton *dealbutton5;
+    QPushButton *communitybutton5;
+
+    void setupUi(QWidget *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(564, 347);
+        selectpage = new QStackedWidget(MainWindow);
+        selectpage->setObjectName("selectpage");
+        selectpage->setGeometry(QRect(20, 10, 531, 331));
+        selectpage1 = new QWidget();
+        selectpage1->setObjectName("selectpage1");
+        groupBox = new QGroupBox(selectpage1);
+        groupBox->setObjectName("groupBox");
+        groupBox->setGeometry(QRect(40, 20, 448, 51));
+        horizontalLayout = new QHBoxLayout(groupBox);
+        horizontalLayout->setObjectName("horizontalLayout");
+        firstbutton1 = new QPushButton(groupBox);
+        firstbutton1->setObjectName("firstbutton1");
+        firstbutton1->setFlat(true);
+
+        horizontalLayout->addWidget(firstbutton1);
+
+        stockbutton1 = new QPushButton(groupBox);
+        stockbutton1->setObjectName("stockbutton1");
+
+        horizontalLayout->addWidget(stockbutton1);
+
+        rankbutton1 = new QPushButton(groupBox);
+        rankbutton1->setObjectName("rankbutton1");
+
+        horizontalLayout->addWidget(rankbutton1);
+
+        dealbutton1 = new QPushButton(groupBox);
+        dealbutton1->setObjectName("dealbutton1");
+
+        horizontalLayout->addWidget(dealbutton1);
+
+        communitybutton1 = new QPushButton(groupBox);
+        communitybutton1->setObjectName("communitybutton1");
+        communitybutton1->setEnabled(true);
+
+        horizontalLayout->addWidget(communitybutton1);
+
+        selectpage->addWidget(selectpage1);
+        selectpage2 = new QWidget();
+        selectpage2->setObjectName("selectpage2");
+        groupBox_2 = new QGroupBox(selectpage2);
+        groupBox_2->setObjectName("groupBox_2");
+        groupBox_2->setGeometry(QRect(40, 20, 448, 51));
+        horizontalLayout_2 = new QHBoxLayout(groupBox_2);
+        horizontalLayout_2->setObjectName("horizontalLayout_2");
+        firstbutton2 = new QPushButton(groupBox_2);
+        firstbutton2->setObjectName("firstbutton2");
+        firstbutton2->setFlat(true);
+
+        horizontalLayout_2->addWidget(firstbutton2);
+
+        stockbutton2 = new QPushButton(groupBox_2);
+        stockbutton2->setObjectName("stockbutton2");
+
+        horizontalLayout_2->addWidget(stockbutton2);
+
+        rankbutton2 = new QPushButton(groupBox_2);
+        rankbutton2->setObjectName("rankbutton2");
+
+        horizontalLayout_2->addWidget(rankbutton2);
+
+        dealbutton2 = new QPushButton(groupBox_2);
+        dealbutton2->setObjectName("dealbutton2");
+
+        horizontalLayout_2->addWidget(dealbutton2);
+
+        communitybutton2 = new QPushButton(groupBox_2);
+        communitybutton2->setObjectName("communitybutton2");
+
+        horizontalLayout_2->addWidget(communitybutton2);
+
+        selectpage->addWidget(selectpage2);
+        selectpage3 = new QWidget();
+        selectpage3->setObjectName("selectpage3");
+        groupBox_3 = new QGroupBox(selectpage3);
+        groupBox_3->setObjectName("groupBox_3");
+        groupBox_3->setGeometry(QRect(40, 20, 448, 51));
+        horizontalLayout_3 = new QHBoxLayout(groupBox_3);
+        horizontalLayout_3->setObjectName("horizontalLayout_3");
+        firstbutton3 = new QPushButton(groupBox_3);
+        firstbutton3->setObjectName("firstbutton3");
+        firstbutton3->setFlat(true);
+
+        horizontalLayout_3->addWidget(firstbutton3);
+
+        stockbutton3 = new QPushButton(groupBox_3);
+        stockbutton3->setObjectName("stockbutton3");
+
+        horizontalLayout_3->addWidget(stockbutton3);
+
+        rankbutton3 = new QPushButton(groupBox_3);
+        rankbutton3->setObjectName("rankbutton3");
+
+        horizontalLayout_3->addWidget(rankbutton3);
+
+        dealbutton3 = new QPushButton(groupBox_3);
+        dealbutton3->setObjectName("dealbutton3");
+
+        horizontalLayout_3->addWidget(dealbutton3);
+
+        communitybutton3 = new QPushButton(groupBox_3);
+        communitybutton3->setObjectName("communitybutton3");
+
+        horizontalLayout_3->addWidget(communitybutton3);
+
+        selectpage->addWidget(selectpage3);
+        selectpage4 = new QWidget();
+        selectpage4->setObjectName("selectpage4");
+        groupBox_4 = new QGroupBox(selectpage4);
+        groupBox_4->setObjectName("groupBox_4");
+        groupBox_4->setGeometry(QRect(40, 20, 448, 51));
+        horizontalLayout_4 = new QHBoxLayout(groupBox_4);
+        horizontalLayout_4->setObjectName("horizontalLayout_4");
+        firstbutton4 = new QPushButton(groupBox_4);
+        firstbutton4->setObjectName("firstbutton4");
+        firstbutton4->setFlat(true);
+
+        horizontalLayout_4->addWidget(firstbutton4);
+
+        stockbutton4 = new QPushButton(groupBox_4);
+        stockbutton4->setObjectName("stockbutton4");
+
+        horizontalLayout_4->addWidget(stockbutton4);
+
+        rankbutton4 = new QPushButton(groupBox_4);
+        rankbutton4->setObjectName("rankbutton4");
+
+        horizontalLayout_4->addWidget(rankbutton4);
+
+        dealbutton4 = new QPushButton(groupBox_4);
+        dealbutton4->setObjectName("dealbutton4");
+
+        horizontalLayout_4->addWidget(dealbutton4);
+
+        communitybutton4 = new QPushButton(groupBox_4);
+        communitybutton4->setObjectName("communitybutton4");
+
+        horizontalLayout_4->addWidget(communitybutton4);
+
+        selectpage->addWidget(selectpage4);
+        selectpage5 = new QWidget();
+        selectpage5->setObjectName("selectpage5");
+        groupBox_5 = new QGroupBox(selectpage5);
+        groupBox_5->setObjectName("groupBox_5");
+        groupBox_5->setGeometry(QRect(40, 20, 448, 51));
+        horizontalLayout_5 = new QHBoxLayout(groupBox_5);
+        horizontalLayout_5->setObjectName("horizontalLayout_5");
+        firstbutton5 = new QPushButton(groupBox_5);
+        firstbutton5->setObjectName("firstbutton5");
+        firstbutton5->setFlat(true);
+
+        horizontalLayout_5->addWidget(firstbutton5);
+
+        stockbutton5 = new QPushButton(groupBox_5);
+        stockbutton5->setObjectName("stockbutton5");
+
+        horizontalLayout_5->addWidget(stockbutton5);
+
+        rankbutton5 = new QPushButton(groupBox_5);
+        rankbutton5->setObjectName("rankbutton5");
+
+        horizontalLayout_5->addWidget(rankbutton5);
+
+        dealbutton5 = new QPushButton(groupBox_5);
+        dealbutton5->setObjectName("dealbutton5");
+
+        horizontalLayout_5->addWidget(dealbutton5);
+
+        communitybutton5 = new QPushButton(groupBox_5);
+        communitybutton5->setObjectName("communitybutton5");
+
+        horizontalLayout_5->addWidget(communitybutton5);
+
+        selectpage->addWidget(selectpage5);
+
+        retranslateUi(MainWindow);
+
+        selectpage->setCurrentIndex(3);
+
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QWidget *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "Form", nullptr));
+        groupBox->setTitle(QString());
+        firstbutton1->setText(QCoreApplication::translate("MainWindow", "\351\246\226\351\241\265", nullptr));
+        stockbutton1->setText(QCoreApplication::translate("MainWindow", "\350\202\241\347\245\250", nullptr));
+        rankbutton1->setText(QCoreApplication::translate("MainWindow", "\346\216\222\350\241\214\346\246\234", nullptr));
+        dealbutton1->setText(QCoreApplication::translate("MainWindow", "\344\272\244\346\230\223\350\247\204\345\210\231", nullptr));
+        communitybutton1->setText(QCoreApplication::translate("MainWindow", "\347\244\276\345\214\272\344\272\244\346\265\201", nullptr));
+        groupBox_2->setTitle(QString());
+        firstbutton2->setText(QCoreApplication::translate("MainWindow", "\351\246\226\351\241\265", nullptr));
+        stockbutton2->setText(QCoreApplication::translate("MainWindow", "\350\202\241\347\245\250", nullptr));
+        rankbutton2->setText(QCoreApplication::translate("MainWindow", "\346\216\222\350\241\214\346\246\234", nullptr));
+        dealbutton2->setText(QCoreApplication::translate("MainWindow", "\344\272\244\346\230\223\350\247\204\345\210\231", nullptr));
+        communitybutton2->setText(QCoreApplication::translate("MainWindow", "\347\244\276\345\214\272\344\272\244\346\265\201", nullptr));
+        groupBox_3->setTitle(QString());
+        firstbutton3->setText(QCoreApplication::translate("MainWindow", "\351\246\226\351\241\265", nullptr));
+        stockbutton3->setText(QCoreApplication::translate("MainWindow", "\350\202\241\347\245\250", nullptr));
+        rankbutton3->setText(QCoreApplication::translate("MainWindow", "\346\216\222\350\241\214\346\246\234", nullptr));
+        dealbutton3->setText(QCoreApplication::translate("MainWindow", "\344\272\244\346\230\223\350\247\204\345\210\231", nullptr));
+        communitybutton3->setText(QCoreApplication::translate("MainWindow", "\347\244\276\345\214\272\344\272\244\346\265\201", nullptr));
+        groupBox_4->setTitle(QString());
+        firstbutton4->setText(QCoreApplication::translate("MainWindow", "\351\246\226\351\241\265", nullptr));
+        stockbutton4->setText(QCoreApplication::translate("MainWindow", "\350\202\241\347\245\250", nullptr));
+        rankbutton4->setText(QCoreApplication::translate("MainWindow", "\346\216\222\350\241\214\346\246\234", nullptr));
+        dealbutton4->setText(QCoreApplication::translate("MainWindow", "\344\272\244\346\230\223\350\247\204\345\210\231", nullptr));
+        communitybutton4->setText(QCoreApplication::translate("MainWindow", "\347\244\276\345\214\272\344\272\244\346\265\201", nullptr));
+        groupBox_5->setTitle(QString());
+        firstbutton5->setText(QCoreApplication::translate("MainWindow", "\351\246\226\351\241\265", nullptr));
+        stockbutton5->setText(QCoreApplication::translate("MainWindow", "\350\202\241\347\245\250", nullptr));
+        rankbutton5->setText(QCoreApplication::translate("MainWindow", "\346\216\222\350\241\214\346\246\234", nullptr));
+        dealbutton5->setText(QCoreApplication::translate("MainWindow", "\344\272\244\346\230\223\350\247\204\345\210\231", nullptr));
+        communitybutton5->setText(QCoreApplication::translate("MainWindow", "\347\244\276\345\214\272\344\272\244\346\265\201", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
