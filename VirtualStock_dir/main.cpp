f02f829759d517c8ef116994aa91866501a27075
lizhuoyou <2938093188@qq.com>
2024-07-10 11:49:36 +0800

    change

--- VirtualStock_dir/main.cpp
+++ VirtualStock_dir/main.cpp
@@ -9,23 +9,20 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-//    UserManage* um = new UserManage;
-//    Global::instance().setGlobalUserManage(um);
-//    BackDB* db = new BackDB;
-//    Global::instance().setGlobalDataBase(db);
-//    ClientSocket* client = new ClientSocket();
-//    Global::instance().setGlobalClient(client);
-//    Login* l = new Login();
-//    l->show();
+   UserManage* um = new UserManage;
+   Global::instance().setGlobalUserManage(um);
+   BackDB* db = new BackDB;
+   Global::instance().setGlobalDataBase(db);
+   ClientSocket* client = new ClientSocket();
+   Global::instance().setGlobalClient(client);
+   Login* l = new Login();
+   l->show();
 
-<<<<<<< HEAD
 
-      BackDB b;
-      b.test();
-=======
-//      BackDB b;
-//      b.test();
->>>>>>> 7a31ade3c1a2e5e255970f42846cc46cf53d9fda
+
+      // BackDB b;
+      // b.test();
+
 
     return a.exec();
 }
