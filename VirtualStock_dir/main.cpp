9b0a97d3c0e50e5d91345a451acf1fc6083d5e3b
Roadinforest <whuForest@outlook.com>
2024-07-10 11:38:48 +0800

    Add three functions about total_price

--- VirtualStock_dir/main.cpp
+++ VirtualStock_dir/main.cpp
@@ -11,18 +11,18 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    UserManage* um = new UserManage;
-    Global::instance().setGlobalUserManage(um);
-    BackDB* db = new BackDB;
-    Global::instance().setGlobalDataBase(db);
-    ClientSocket* client = new ClientSocket();
-    Global::instance().setGlobalClient(client);
-    Login* l = new Login();
-    l->show();
+//    UserManage* um = new UserManage;
+//    Global::instance().setGlobalUserManage(um);
+//    BackDB* db = new BackDB;
+//    Global::instance().setGlobalDataBase(db);
+//    ClientSocket* client = new ClientSocket();
+//    Global::instance().setGlobalClient(client);
+//    Login* l = new Login();
+//    l->show();
 
 
-//      BackDB b;
-//      b.test();
+      BackDB b;
+      b.test();
 
     return a.exec();
 }
