a51f89b8b032d685103e18abad123726bd791603
riytryror <3141773287@qq.com>
2024-07-11 16:29:35 +0800

    forum refresh update

--- VirtualStock_dir/forum.cpp
+++ VirtualStock_dir/forum.cpp
@@ -23,12 +23,6 @@ forum::forum(QWidget *parent):QWidget(parent){
             }
         }
     }
-
-
-
-
-
-
     // 创建一个QGridLayout来管理按钮布局
     buttonLayout = new QGridLayout(this);
 
@@ -78,12 +72,7 @@ forum::forum(QWidget *parent):QWidget(parent){
             ttemp="本帖发布时间为2023年"+QString::number(num)+"月";}
         else{
             ttemp="本帖发布时间为用户模拟结束后";}
-<<<<<<< HEAD
-        button = new QPushButton(temp+"\n"+"发帖人："+load[m-row-1].getid()+"\n"+load[m-row-1].getcontent()+"\n"+ttemp, this);
-        // button->setFixedHeight(300);
-        button->setFixedWidth(600);
-        button->setStyleSheet("QPushButton { font-size:32px }");
-=======
+
         QString fix;
         int l=load[m-row-1].getcontent().size();
         for(int i=0;i<l;i+=40){
@@ -93,7 +82,9 @@ forum::forum(QWidget *parent):QWidget(parent){
         button = new QPushButton(temp+"\n"+"发帖人："+load[m-row-1].getid()+"\n"+fix+"\n"+ttemp, this);
         button->setFixedHeight(300);
         button->setStyleSheet("QPushButton { font-size:32px}");
->>>>>>> 19faf1629a4b90af9985c66ef6674722be0de65c
+
+
+
         buttonLayout->addWidget(button, row+1, 0,1,3);
         // 连接clicked信号到对应的槽函数
         connect(button, &QPushButton::clicked, this, &forum::detail);
@@ -106,6 +97,7 @@ forum::forum(QWidget *parent):QWidget(parent){
     //vks
 
     ui->setupUi(this);
+    showMaximized();
 }
 
 forum::~forum()
@@ -187,92 +179,9 @@ void forum::display(int m){
 }
 
 void forum::refresh(){
-    time=Global:: instance().getGlobalDataBase()->getTime(Global::instance().getGlobalUserManage()->GetUser(0)->GetId());
-    std::vector<Post>_load;//这里应当重新获取数据库中的表格
-    _load=Global::instance().getGlobalDataBase()->getforum();
-    allnumber=_load.size();
-    for(int i=0;i<allnumber;i++){
-        if(_load[i].getdate()>time){continue;}
-        if(_load[i].getfatherid()==_load[i].getthisid()){
-            load.push_back(_load[i]);
-            number.push_back(_load[i].getfatherid());
-            son.push_back(*new std::vector<Post>);
-        }
-        else{
-            int m=number.size();
-            for(int ii=0;ii<m;ii++){
-                if(_load[i].getfatherid()==number[ii]){
-                    son[ii].push_back(_load[i]);
-                    break;
-                }
-            }
-        }
-    }
-
-    // 创建一个QGridLayout来管理按钮布局
-    buttonLayout = new QGridLayout(this);
-
-    //创建滚动
-    QScrollArea *scrollArea = new QScrollArea();
-    scrollArea->setWidgetResizable(true); // 设置滚动区域可以调整大小
-
-    // 创建一个QWidget作为滚动区域的子窗口
-    QWidget *scrollAreaWidgetContents = new QWidget();
-    scrollAreaWidgetContents->setLayout(buttonLayout); // 将网格布局设置到这个QWidget上
-
-    // 将QWidget设置到滚动区域中
-    scrollArea->setWidget(scrollAreaWidgetContents);
-
-    // 创建一个布局并将滚动区域添加到主窗口中
-    QVBoxLayout *mainLayout = new QVBoxLayout(this);
-    mainLayout->addWidget(scrollArea);
-
-    // 创建输入文本框
-    QLineEdit *lineEdit = new QLineEdit();
-    buttonLayout->addWidget(lineEdit, 0, 0); // 将文本框添加到第一行第一列
-
-    // 创建提交按钮
-    QPushButton *submitButton = new QPushButton("我要发新帖");
-    buttonLayout->addWidget(submitButton, 0, 1); // 将提交按钮添加到第一行第二列
-
-    // 连接提交按钮的clicked信号到槽函数
-    connect(submitButton, &QPushButton::clicked, this, &forum::onSubmitClicked);
-
-    QPushButton *refreashButton = new QPushButton("刷新");
-    buttonLayout->addWidget(refreashButton, 0, 2); // 将提交按钮添加到第一行第二列
-    connect(refreashButton, &QPushButton::clicked, this, &forum::refresh);
-
-    // 动态创建按钮并添加到布局中
-
-
-    int m=number.size();
-    for (int row = 0; row < m; ++row) {
-        // 创建按钮
-        QPushButton *button ;
-        QString temp="显示"+QString::number(m-row)+"号帖子详情";
-        QString ttemp;
-
-        int num=load[m-row-1].getdate();
-        if(num!=13){
-            ttemp="本帖发布时间为2023年"+QString::number(num)+"月";}
-        else{
-            ttemp="本帖发布时间为用户模拟结束后";}
-        QString fix;
-        int l=load[m-row-1].getcontent().size();
-        for(int i=0;i<l;i+=40){
-            fix+=load[m-row-1].getcontent().mid(i,40);
-            fix+="\n";
-        }
-        button = new QPushButton(temp+"\n"+"发帖人："+load[m-row-1].getid()+"\n"+fix+"\n"+ttemp, this);
-        button->setFixedHeight(300);
-        buttonLayout->addWidget(button, row+1, 0,1,3);
-        // 连接clicked信号到对应的槽函数
-        connect(button, &QPushButton::clicked, this, &forum::detail);
-
-
-    }
     forum*newforum=new forum();
     newforum->show();
     this->close();
     qDebug() << "vks-server: refresh success";
 }
+
